6673a2540b8b3dbc7cd8565670251f721d63b8c9
Davo Smith <git@davosmith.co.uk>
2013-04-29 09:42:15 +0100

    block_dlbnavi - add missing addinstance permission

--- blocks/dlbnavi/db/access.php
+++ blocks/dlbnavi/db/access.php
@@ -33,6 +33,19 @@ $capabilities = array(
         ),
         'clonepermissionsfrom' => 'moodle/my:manageblocks'
     ),
+
+    'block/dlbnavi:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
 );
 
 

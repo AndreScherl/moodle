e32345845f6130e7276d09eb782f382281b6de31
Davo Smith <git@davosmith.co.uk>
2013-05-15 09:39:33 +0100

    ALP-55 block_meineschulen - adjust layout of blocks to be 75% 25% (not 50% 50%)

--- blocks/meineschulen/version.php
+++ blocks/meineschulen/version.php
@@ -24,12 +24,12 @@
  
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013051500;
+$plugin->version   = 2013051501;
 $plugin->requires  = 2012120300; // 2.4
 $plugin->cron      = 0;
 $plugin->component = 'block_meineschulen';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '2.4 (Build: 2013051500)';
+$plugin->release   = '2.4 (Build: 2013051501)';
 $plugin->dependencies = array(
     'block_meinekurse' => ANY_VERSION,
 );

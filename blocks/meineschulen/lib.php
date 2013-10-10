4557ea01c6e5a341da75bf1070fc7cd3a042de8d
Davo Smith <git@davosmith.co.uk>
2013-10-10 13:05:19 +0100

    ALP-73 block_meineschulen - added extra debugging to log why a user can see the 'request course' button

--- blocks/meineschulen/lib.php
+++ blocks/meineschulen/lib.php
@@ -237,15 +237,42 @@ class meineschulen {
                 $resp = true;
             } else {
                 $roles = get_roles_with_capability('moodle/course:request');
+                self::debuglog("Checking if current user ($USER->id) can request courses - roles that can request are: ");
+                foreach ($roles as $role) {
+                    self::debuglog("{$role->shortname} ({$role->id})");
+                }
                 if ($roles) {
                     list($rsql, $params) = $DB->get_in_or_equal(array_keys($roles), SQL_PARAMS_NAMED);
                     $params['userid'] = $USER->id;
                     $resp = $DB->record_exists_select('role_assignments', "userid = :userid AND roleid $rsql", $params);
+
+                    self::debuglog("User has roles: ");
+                    self::debuglog($DB->get_fieldset_select('role_assignments', 'DISTINCT(roleid)', "userid = ?", array($USER->id)));
                 }
             }
         }
 
         return $resp;
+    }
+
+    protected static function debuglog($msg) {
+        global $CFG;
+
+        if (is_object($msg)) {
+            ob_start();
+            print_r($msg);
+            $msg = ob_get_clean();
+        }
+        if (is_array($msg)) {
+            $msg = implode(',', $msg);
+        }
+
+        $fp = fopen($CFG->dataroot.'/meineschulen.log', 'a');
+        if (!$fp) {
+            return;
+        }
+        fwrite($fp, $msg."\n");
+        fclose($fp);
     }
 
     /**

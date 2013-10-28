0c490b075c5c6990a11ce241aeb1f62748d424da
Andrea.Taras <a.taras@alp.dillingen.de>
2013-10-28 16:41:59 +0100

    atar: added German language file for Mindmap module

---
+++ mod/mindmap/lang/de/mindmap.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of Mindmap module for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Mindmap plugin language strings
+ *
+ * @package    mod
+ * @subpackage mindmap
+ * @author ekpenso.com
+ * @copyright  2011 Tõnis Tartes <tonis.tartes@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Mindmap';
+$string['pluginadministration'] = 'Mindmap Verwaltung';
+$string['index_help'] = 'Das Mindmap Module wird von <a href="http://ekpenso.com">ekpenso.com</a> bereitgestellt - bitte kontaktieren Sie uns bei Problemen oder wenn Sie uns Rückmeldung geben wollen.';
+$string['mindmap_help'] = 'Das Mindmap Module wird von <a href="http://ekpenso.com">ekpenso.com</a> bereitgestellt - bitte kontaktieren Sie uns bei Problemen oder wenn Sie uns Rückmeldung geben wollen.';
+$string['mindmap'] = 'Mindmap';
+$string['modulename'] = 'Mindmap';
+$string['modulename_help'] = 'Die Mindmap-Aktivität stellt eine Mindmap zur Verfügung.';
+$string['modulenameplural'] = 'Mindmaps';
+$string['mindmapfieldset'] = 'Custom example fieldset';
+$string['mindmapintro'] = 'Mindmap Intro';
+$string['mindmapname'] = 'Mindmap Name';
+$string['editable'] = 'Schüler dürfen diese Mindmap bearbeiten';
+$string['mindmaphint'] = 'Um einen neuen Knoten hinzuzufügen, drücken Sie "Einfügen" auf Ihrer Tastatur';
+$string['mindmap:addinstance'] = 'Fügen Sie eine neue Mindmap-Instanz hinzu';
+$string['mindmaplocked'] = 'Sie dürfen diese Mindmap nicht bearbeiten, da Benutzer {$a->firstname} {$a->lastname} sie gerade editiert';
+$string['mindmapunlocked'] = 'Wenn Sie diese Seite verlassen, ist die Mindmap für andere zur Bearbeitung freigegeben';
+$string['locking'] = 'Gleichzeitige Bearbeitung verbieten';
+
+?>

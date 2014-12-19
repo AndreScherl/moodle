7d15e4f01fedf3f0196d5e8ae14266fb579e23b0
Andre Scherl <andre.scherl@isb.bayern.de>
2014-12-19 15:44:25 +0100

    block mbs_my_courses: removed title links (request course, create course)

--- blocks/mbs_my_courses/renderer.php
+++ blocks/mbs_my_courses/renderer.php
@@ -331,25 +331,6 @@ class block_mbs_my_courses_renderer extends plugin_renderer_base {
     }
 
     /**
-     * Brings create course link to blocks header (Note: points to the first of users categories list)
-     *
-     * @return string return the HTML as a string, rather than printing it.
-     */
-    public function header_with_links() {
-        global $USER;
-        $title = get_string("pluginname", "block_mbs_my_courses");
-        $create_course_link = '';
-        if(mbs_my_courses::can_create_some_course($USER->id)) {
-            $create_course_link .= html_writer::tag("a", get_string("create_course", "block_mbs_my_courses"), array("class" => "mbs_my_courses_createcourse_link", "href" => new moodle_url("/course/edit.php", array("category" => mbs_my_courses::schools_of_user()[0]->id, "returnto" => "category"))));
-        }
-        $request_course_link = '';
-        if(mbs_my_courses::can_request_home_course()) {
-            $request_course_link .= html_writer::tag("a", get_string("request_course", "block_mbs_my_courses"), array("class" => "mbs_my_courses_createcourse_link", "href" => new moodle_url("/blocks/meineschulen/request.php", array("category" => mbs_my_courses::schools_of_user()[0]->id))));
-        }
-
-        return $title . $create_course_link . $request_course_link;
-    }
-    /**
      * Construct button to load more results
      *
      * @return string return the HTML as a string, rather than printing it.

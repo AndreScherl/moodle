153f820b4de9524a53c523c2c53f7bff78b145d0
Andre Scherl <andre.scherl@isb.bayern.de>
2015-02-17 17:40:59 +0100

    enrol_mbsteamteaching: simplified edit form (only password field)

--- enrol/mbsteamteaching/edit_form.php
+++ enrol/mbsteamteaching/edit_form.php
@@ -38,18 +38,18 @@ class enrol_mbsteamteaching_edit_form extends moodleform {
 
         $mform->addElement('header', 'header', get_string('pluginname', 'enrol_mbsteamteaching'));
 
-        $mform->addElement('text', 'name', get_string('custominstancename', 'enrol'));
-        $mform->setType('name', PARAM_TEXT);
+        // $mform->addElement('text', 'name', get_string('custominstancename', 'enrol'));
+        // $mform->setType('name', PARAM_TEXT);
 
-        $options = array(ENROL_INSTANCE_ENABLED  => get_string('yes'),
-                         ENROL_INSTANCE_DISABLED => get_string('no'));
-        $mform->addElement('select', 'status', get_string('status', 'enrol_mbsteamteaching'), $options);
-        $mform->addHelpButton('status', 'status', 'enrol_mbsteamteaching');
+        // $options = array(ENROL_INSTANCE_ENABLED  => get_string('yes'),
+        //                  ENROL_INSTANCE_DISABLED => get_string('no'));
+        // $mform->addElement('select', 'status', get_string('status', 'enrol_mbsteamteaching'), $options);
+        // $mform->addHelpButton('status', 'status', 'enrol_mbsteamteaching');
 
-        $options = array(1 => get_string('yes'), 0 => get_string('no'));
-        $mform->addElement('select', 'customint6', get_string('newenrols', 'enrol_mbsteamteaching'), $options);
-        $mform->addHelpButton('customint6', 'newenrols', 'enrol_mbsteamteaching');
-        $mform->disabledIf('customint6', 'status', 'eq', ENROL_INSTANCE_DISABLED);
+        // $options = array(1 => get_string('yes'), 0 => get_string('no'));
+        // $mform->addElement('select', 'customint6', get_string('newenrols', 'enrol_mbsteamteaching'), $options);
+        // $mform->addHelpButton('customint6', 'newenrols', 'enrol_mbsteamteaching');
+        // $mform->disabledIf('customint6', 'status', 'eq', ENROL_INSTANCE_DISABLED);
 
         $mform->addElement('passwordunmask', 'password', get_string('password', 'enrol_mbsteamteaching'));
         $mform->addHelpButton('password', 'password', 'enrol_mbsteamteaching');
@@ -57,90 +57,90 @@ class enrol_mbsteamteaching_edit_form extends moodleform {
             $mform->addRule('password', get_string('required'), 'required', null, 'client');
         }
 
-        $options = array(1 => get_string('yes'),
-                         0 => get_string('no'));
-        $mform->addElement('select', 'customint1', get_string('groupkey', 'enrol_mbsteamteaching'), $options);
-        $mform->addHelpButton('customint1', 'groupkey', 'enrol_mbsteamteaching');
+        // $options = array(1 => get_string('yes'),
+        //                  0 => get_string('no'));
+        // $mform->addElement('select', 'customint1', get_string('groupkey', 'enrol_mbsteamteaching'), $options);
+        // $mform->addHelpButton('customint1', 'groupkey', 'enrol_mbsteamteaching');
 
-        $roles = $this->extend_assignable_roles($context, $instance->roleid);
-        $mform->addElement('select', 'roleid', get_string('role', 'enrol_mbsteamteaching'), $roles);
+        // $roles = $this->extend_assignable_roles($context, $instance->roleid);
+        // $mform->addElement('select', 'roleid', get_string('role', 'enrol_mbsteamteaching'), $roles);
 
-        $mform->addElement('duration', 'enrolperiod', get_string('enrolperiod', 'enrol_mbsteamteaching'), array('optional' => true, 'defaultunit' => 86400));
-        $mform->addHelpButton('enrolperiod', 'enrolperiod', 'enrol_mbsteamteaching');
+        // $mform->addElement('duration', 'enrolperiod', get_string('enrolperiod', 'enrol_mbsteamteaching'), array('optional' => true, 'defaultunit' => 86400));
+        // $mform->addHelpButton('enrolperiod', 'enrolperiod', 'enrol_mbsteamteaching');
 
-        $options = array(0 => get_string('no'), 1 => get_string('expirynotifyenroller', 'core_enrol'), 2 => get_string('expirynotifyall', 'core_enrol'));
-        $mform->addElement('select', 'expirynotify', get_string('expirynotify', 'core_enrol'), $options);
-        $mform->addHelpButton('expirynotify', 'expirynotify', 'core_enrol');
+        // $options = array(0 => get_string('no'), 1 => get_string('expirynotifyenroller', 'core_enrol'), 2 => get_string('expirynotifyall', 'core_enrol'));
+        // $mform->addElement('select', 'expirynotify', get_string('expirynotify', 'core_enrol'), $options);
+        // $mform->addHelpButton('expirynotify', 'expirynotify', 'core_enrol');
 
-        $mform->addElement('duration', 'expirythreshold', get_string('expirythreshold', 'core_enrol'), array('optional' => false, 'defaultunit' => 86400));
-        $mform->addHelpButton('expirythreshold', 'expirythreshold', 'core_enrol');
-        $mform->disabledIf('expirythreshold', 'expirynotify', 'eq', 0);
+        // $mform->addElement('duration', 'expirythreshold', get_string('expirythreshold', 'core_enrol'), array('optional' => false, 'defaultunit' => 86400));
+        // $mform->addHelpButton('expirythreshold', 'expirythreshold', 'core_enrol');
+        // $mform->disabledIf('expirythreshold', 'expirynotify', 'eq', 0);
 
-        $mform->addElement('date_time_selector', 'enrolstartdate', get_string('enrolstartdate', 'enrol_mbsteamteaching'), array('optional' => true));
-        $mform->setDefault('enrolstartdate', 0);
-        $mform->addHelpButton('enrolstartdate', 'enrolstartdate', 'enrol_mbsteamteaching');
+        // $mform->addElement('date_time_selector', 'enrolstartdate', get_string('enrolstartdate', 'enrol_mbsteamteaching'), array('optional' => true));
+        // $mform->setDefault('enrolstartdate', 0);
+        // $mform->addHelpButton('enrolstartdate', 'enrolstartdate', 'enrol_mbsteamteaching');
 
-        $mform->addElement('date_time_selector', 'enrolenddate', get_string('enrolenddate', 'enrol_mbsteamteaching'), array('optional' => true));
-        $mform->setDefault('enrolenddate', 0);
-        $mform->addHelpButton('enrolenddate', 'enrolenddate', 'enrol_mbsteamteaching');
+        // $mform->addElement('date_time_selector', 'enrolenddate', get_string('enrolenddate', 'enrol_mbsteamteaching'), array('optional' => true));
+        // $mform->setDefault('enrolenddate', 0);
+        // $mform->addHelpButton('enrolenddate', 'enrolenddate', 'enrol_mbsteamteaching');
 
-        $options = array(0 => get_string('never'),
-                 1800 * 3600 * 24 => get_string('numdays', '', 1800),
-                 1000 * 3600 * 24 => get_string('numdays', '', 1000),
-                 365 * 3600 * 24 => get_string('numdays', '', 365),
-                 180 * 3600 * 24 => get_string('numdays', '', 180),
-                 150 * 3600 * 24 => get_string('numdays', '', 150),
-                 120 * 3600 * 24 => get_string('numdays', '', 120),
-                 90 * 3600 * 24 => get_string('numdays', '', 90),
-                 60 * 3600 * 24 => get_string('numdays', '', 60),
-                 30 * 3600 * 24 => get_string('numdays', '', 30),
-                 21 * 3600 * 24 => get_string('numdays', '', 21),
-                 14 * 3600 * 24 => get_string('numdays', '', 14),
-                 7 * 3600 * 24 => get_string('numdays', '', 7));
-        $mform->addElement('select', 'customint2', get_string('longtimenosee', 'enrol_mbsteamteaching'), $options);
-        $mform->addHelpButton('customint2', 'longtimenosee', 'enrol_mbsteamteaching');
+        // $options = array(0 => get_string('never'),
+        //          1800 * 3600 * 24 => get_string('numdays', '', 1800),
+        //          1000 * 3600 * 24 => get_string('numdays', '', 1000),
+        //          365 * 3600 * 24 => get_string('numdays', '', 365),
+        //          180 * 3600 * 24 => get_string('numdays', '', 180),
+        //          150 * 3600 * 24 => get_string('numdays', '', 150),
+        //          120 * 3600 * 24 => get_string('numdays', '', 120),
+        //          90 * 3600 * 24 => get_string('numdays', '', 90),
+        //          60 * 3600 * 24 => get_string('numdays', '', 60),
+        //          30 * 3600 * 24 => get_string('numdays', '', 30),
+        //          21 * 3600 * 24 => get_string('numdays', '', 21),
+        //          14 * 3600 * 24 => get_string('numdays', '', 14),
+        //          7 * 3600 * 24 => get_string('numdays', '', 7));
+        // $mform->addElement('select', 'customint2', get_string('longtimenosee', 'enrol_mbsteamteaching'), $options);
+        // $mform->addHelpButton('customint2', 'longtimenosee', 'enrol_mbsteamteaching');
 
-        $mform->addElement('text', 'customint3', get_string('maxenrolled', 'enrol_mbsteamteaching'));
-        $mform->addHelpButton('customint3', 'maxenrolled', 'enrol_mbsteamteaching');
-        $mform->setType('customint3', PARAM_INT);
+        // $mform->addElement('text', 'customint3', get_string('maxenrolled', 'enrol_mbsteamteaching'));
+        // $mform->addHelpButton('customint3', 'maxenrolled', 'enrol_mbsteamteaching');
+        // $mform->setType('customint3', PARAM_INT);
 
-        $cohorts = array(0 => get_string('no'));
-        list($sqlparents, $params) = $DB->get_in_or_equal($context->get_parent_context_ids(), SQL_PARAMS_NAMED);
-        $params['current'] = $instance->customint5;
-        $sql = "SELECT id, name, idnumber, contextid
-                  FROM {cohort}
-                 WHERE contextid $sqlparents OR id = :current
-              ORDER BY name ASC, idnumber ASC";
-        $rs = $DB->get_recordset_sql($sql, $params);
-        foreach ($rs as $c) {
-            $ccontext = context::instance_by_id($c->contextid);
-            if ($c->id != $instance->customint5 and !has_capability('moodle/cohort:view', $ccontext)) {
-                continue;
-            }
-            $cohorts[$c->id] = format_string($c->name, true, array('context'=>$context));
-            if ($c->idnumber) {
-                $cohorts[$c->id] .= ' ['.s($c->idnumber).']';
-            }
-        }
-        if (!isset($cohorts[$instance->customint5])) {
-            // Somebody deleted a cohort, better keep the wrong value so that random ppl can not enrol.
-            $cohorts[$instance->customint5] = get_string('unknowncohort', 'cohort', $instance->customint5);
-        }
-        $rs->close();
-        if (count($cohorts) > 1) {
-            $mform->addElement('select', 'customint5', get_string('cohortonly', 'enrol_mbsteamteaching'), $cohorts);
-            $mform->addHelpButton('customint5', 'cohortonly', 'enrol_mbsteamteaching');
-        } else {
-            $mform->addElement('hidden', 'customint5');
-            $mform->setType('customint5', PARAM_INT);
-            $mform->setConstant('customint5', 0);
-        }
+        // $cohorts = array(0 => get_string('no'));
+        // list($sqlparents, $params) = $DB->get_in_or_equal($context->get_parent_context_ids(), SQL_PARAMS_NAMED);
+        // $params['current'] = $instance->customint5;
+        // $sql = "SELECT id, name, idnumber, contextid
+        //           FROM {cohort}
+        //          WHERE contextid $sqlparents OR id = :current
+        //       ORDER BY name ASC, idnumber ASC";
+        // $rs = $DB->get_recordset_sql($sql, $params);
+        // foreach ($rs as $c) {
+        //     $ccontext = context::instance_by_id($c->contextid);
+        //     if ($c->id != $instance->customint5 and !has_capability('moodle/cohort:view', $ccontext)) {
+        //         continue;
+        //     }
+        //     $cohorts[$c->id] = format_string($c->name, true, array('context'=>$context));
+        //     if ($c->idnumber) {
+        //         $cohorts[$c->id] .= ' ['.s($c->idnumber).']';
+        //     }
+        // }
+        // if (!isset($cohorts[$instance->customint5])) {
+        //     // Somebody deleted a cohort, better keep the wrong value so that random ppl can not enrol.
+        //     $cohorts[$instance->customint5] = get_string('unknowncohort', 'cohort', $instance->customint5);
+        // }
+        // $rs->close();
+        // if (count($cohorts) > 1) {
+        //     $mform->addElement('select', 'customint5', get_string('cohortonly', 'enrol_mbsteamteaching'), $cohorts);
+        //     $mform->addHelpButton('customint5', 'cohortonly', 'enrol_mbsteamteaching');
+        // } else {
+        //     $mform->addElement('hidden', 'customint5');
+        //     $mform->setType('customint5', PARAM_INT);
+        //     $mform->setConstant('customint5', 0);
+        // }
 
-        $mform->addElement('advcheckbox', 'customint4', get_string('sendcoursewelcomemessage', 'enrol_mbsteamteaching'));
-        $mform->addHelpButton('customint4', 'sendcoursewelcomemessage', 'enrol_mbsteamteaching');
+        // $mform->addElement('advcheckbox', 'customint4', get_string('sendcoursewelcomemessage', 'enrol_mbsteamteaching'));
+        // $mform->addHelpButton('customint4', 'sendcoursewelcomemessage', 'enrol_mbsteamteaching');
 
-        $mform->addElement('textarea', 'customtext1', get_string('customwelcomemessage', 'enrol_mbsteamteaching'), array('cols'=>'60', 'rows'=>'8'));
-        $mform->addHelpButton('customtext1', 'customwelcomemessage', 'enrol_mbsteamteaching');
+        // $mform->addElement('textarea', 'customtext1', get_string('customwelcomemessage', 'enrol_mbsteamteaching'), array('cols'=>'60', 'rows'=>'8'));
+        // $mform->addHelpButton('customtext1', 'customwelcomemessage', 'enrol_mbsteamteaching');
 
         $mform->addElement('hidden', 'id');
         $mform->setType('id', PARAM_INT);
@@ -164,15 +164,15 @@ class enrol_mbsteamteaching_edit_form extends moodleform {
         $checkpassword = false;
 
         if ($instance->id) {
-            if ($data['status'] == ENROL_INSTANCE_ENABLED) {
+            // if ($data['status'] == ENROL_INSTANCE_ENABLED) {
                 if ($instance->password !== $data['password']) {
                     $checkpassword = true;
                 }
-            }
+            // }
         } else {
-            if ($data['status'] == ENROL_INSTANCE_ENABLED) {
+            // if ($data['status'] == ENROL_INSTANCE_ENABLED) {
                 $checkpassword = true;
-            }
+            // }
         }
 
         if ($checkpassword) {
@@ -188,15 +188,15 @@ class enrol_mbsteamteaching_edit_form extends moodleform {
             }
         }
 
-        if ($data['status'] == ENROL_INSTANCE_ENABLED) {
-            if (!empty($data['enrolenddate']) and $data['enrolenddate'] < $data['enrolstartdate']) {
-                $errors['enrolenddate'] = get_string('enrolenddaterror', 'enrol_mbsteamteaching');
-            }
-        }
+        // if ($data['status'] == ENROL_INSTANCE_ENABLED) {
+        //     if (!empty($data['enrolenddate']) and $data['enrolenddate'] < $data['enrolstartdate']) {
+        //         $errors['enrolenddate'] = get_string('enrolenddaterror', 'enrol_mbsteamteaching');
+        //     }
+        // }
 
-        if ($data['expirynotify'] > 0 and $data['expirythreshold'] < 86400) {
-            $errors['expirythreshold'] = get_string('errorthresholdlow', 'core_enrol');
-        }
+        // if ($data['expirynotify'] > 0 and $data['expirythreshold'] < 86400) {
+        //     $errors['expirythreshold'] = get_string('errorthresholdlow', 'core_enrol');
+        // }
 
         return $errors;
     }

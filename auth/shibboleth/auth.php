a7acb9569fe2a4e66202161cfd8eff717e42dc83
Andre Scherl <andre.scherl@isb.bayern.de>
2015-04-08 14:09:41 +0200

    bugfix: the last wrong session var

--- auth/shibboleth/auth.php
+++ auth/shibboleth/auth.php
@@ -75,7 +75,7 @@ class auth_plugin_shibboleth extends auth_plugin_base {
 
             //+++ Andre Scherl, add conditions to restrict the access of the mebis beta lms
             if($_SERVER["Shib-Application-ID"] == "beta") {
-                return ((strtolower($_SERVER[$this->config->user_attribute]) == strtolower($username)) && ($_SESSION["mebisBetaAccess"] == "TRUE"));
+                return ((strtolower($_SERVER[$this->config->user_attribute]) == strtolower($username)) && ($_SERVER["mebisBetaAccess"] == "TRUE"));
             }
             //---
 

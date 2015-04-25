b073237407b978ee7a88e1eea51f42be6304b2d1
Andreas Wagner <awagner@gmx.de>
2015-04-25 09:32:07 +0200

    adding pimped courses report

---
+++ report/mbs/yui/src/toggleinfo/js/toggleinfo.js
@@ -0,0 +1,27 @@
+M.report_mbs = M.report_mbs || {};
+M.report_mbs.toggleinit = function (data) {
+
+    function initialize() {
+
+        var infobuttons = Y.all('img[id^="info"]');
+
+        if (infobuttons) {
+
+            infobuttons.each(
+
+                function(node, index) {
+
+                    var courseid = Number(node.get('id').split('_')[1]);
+
+                    node.on('click',
+                        function (e) {
+
+                            e.preventDefault();
+                            Y.one('#content_' + courseid).toggleView();
+                        });
+                });
+        }
+    }
+
+    initialize();
+};

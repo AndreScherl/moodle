80ca7aecda7366a8a0e014b4f815920c72cd51d8
Franziska Hübler <franziska.huebler@isb.bayern.de>
2015-05-26 16:03:36 +0200

    block mbsschooltitle: * removed me-event-footer * styled block

--- theme/mebis/lang/en/theme_mebis.php
+++ theme/mebis/lang/en/theme_mebis.php
@@ -66,7 +66,6 @@ $string['footer-terms_of_use'] = 'Nutzungsbedingungen';
 $string['footer-search-schooltypes'] = 'Pruefungstermine für alle Schularten';
 $string['footer-search-course_or_school'] = 'Kurs oder Schule suchen';
 $string['footer-search-media'] = 'Medien suchen oder Suchbegriff eingeben';
-$string['footer-my-school'] = 'Meine Schule';
 
 $string['menu-administration-head'] = 'Lernplattform-Verwaltung';
 $string['menu-administration-link'] = 'Webseiten Administration';

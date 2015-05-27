77d68d1a7cf8afa529e07af3edb6ba06fb0b349c
Andre Scherl <andre.scherl@isb.bayern.de>
2015-05-27 16:25:50 +0200

    finished mbswizzard incl the first two sequences

--- blocks/mbswizzard/js/wizzard.js
+++ blocks/mbswizzard/js/wizzard.js
@@ -36,6 +36,10 @@ M.block_mbswizzard.wizzard.init = function() {
     // e.g. course_create and mbswizzard_sequence_course_create.json
     $('.link_wizzard').on('click', $.proxy(function(event){
         this.event = event;
+        // no event handling on static preview links. Remove this if new wizzard is available!
+        if ($(event.target).attr("data-wizzard") === "first_learningsequence") {
+            return;
+        }
 	var seqname = "mbswizzard_sequence_"+$(event.target).attr("data-wizzard");
   	this.copy_sequence_from_json(seqname, $.proxy(function(success){
             if (success) {
@@ -61,13 +65,21 @@ M.block_mbswizzard.wizzard.init = function() {
 	this.show_tip(this.sequence.current_step);
         	
         // Prepare next step
-	this.prepare_next_step(this.sequence.current_step);
+	//this.prepare_next_step(this.sequence.current_step);
         
         //Listen to cancel button
         $('div[data-block=block_mbswizzard] .cancel').on('click', $.proxy(function() {
             this.finish_sequence('cancel')
         }, this));
     }
+    
+    // Set static tooltip for upcoming wizzards. Remove this, if new wizzard is available.
+    $('a[data-wizzard=first_learningsequence]').tooltip({
+        // trigger: 'click',
+        animation: true,
+        title: 'Folgt in Kürze.'
+    });
+    
 };
 
 /*
@@ -117,14 +129,33 @@ M.block_mbswizzard.wizzard.show_tip = function(step) {
         $(this.sequence.steps[step-1].sel).tooltip('destroy');
     }
         
-    // Show the current tooltip.
     var cs = this.sequence.steps[step];
+    
+    // Set visibility if element is hidden
+    if($(cs.sel).is(':hidden')) {
+        $(cs.sel).css("visibility", "visible");
+    }
+    
+    // Show the current tooltip.
+    var placement = "auto";
+    if (cs.placement != null) {
+        placement = cs.placement;
+    }
+    var viewport = { selector: 'body', padding: 0 };
+    if(cs.viewport != null) {
+        viewport = cs.viewport;
+    }
+    
     $(cs.sel).tooltip({
 	html: true,
 	title: cs.tip,
-	trigger: 'manual'
+	trigger: 'manual',
+        placement: placement,
+        animation: true,
+        viewport: viewport
     });
-    $(cs.sel).tooltip('show');
+    $(cs.sel).first().tooltip('show');
+    this.prepare_next_step(step);
     
     var offsettop = $('#topbar').height() + $('header.me-page-header.full').height() + 150;
     $.scrollTo(cs.sel, 1000, {
@@ -138,24 +169,32 @@ M.block_mbswizzard.wizzard.show_tip = function(step) {
  * Prepare the next step
  * @param int cs - current step
  */
-M.block_mbswizzard.wizzard.prepare_next_step = function(cs) {
-    if (this.sequence.steps.length == cs+1) {
+M.block_mbswizzard.wizzard.prepare_next_step = function(cstep) { 
+    if (this.sequence.steps.length == cstep+1) {
 	// the end, there is no next step
         this.prepare_finish_sequence('finish');
 	return;
     }
-	
-    if (window.location.pathname.search(this.sequence.steps[cs+1].url) != -1) {
-	// next link of tip
-	$('.wizzard_next_step').on('click', $.proxy(function(e) {
-            this.sequence.current_step = cs+1;
+    
+    // If there is an action selector to get to the next step, take it. Otherwise listen to the origin target.
+    // Only needed, if you stay on the same page
+    if (this.sequence.steps[cstep].actionsel != null) {
+	$(this.sequence.steps[cstep].actionsel).on('click', $.proxy(function(e) {
+            this.sequence.current_step = cstep+1;
             this.store_sequence(this.sequence);
-            this.show_tip(cs+1);
-            this.prepare_next_step(cs+1);
+            // if next element isn't on the page the right way immediately, wait a bit
+            if($(this.sequence.steps[cstep+1].sel).length == 0 || $(this.sequence.steps[cstep+1].sel).is(':hidden')) {
+                setTimeout($.proxy(function(){
+                    this.show_tip(cstep+1);
+                }, this), 500);
+            } else {
+                this.show_tip(cstep+1);
+            }
+            this.prepare_next_step(cstep+1);
 	}, this));
     } else {
-	$(this.sequence.steps[cs].sel).on('click', $.proxy(function(e) {
-            this.sequence.current_step = cs+1;
+	$(this.sequence.steps[cstep].sel).on('click', $.proxy(function(e) {
+            this.sequence.current_step = cstep+1;
             this.store_sequence(this.sequence);
 	}, this)); 
     }
@@ -196,9 +235,9 @@ M.block_mbswizzard.wizzard.update_progressbar = function() {
  * 
  * @param string $state - finish or cancel
  */
-M.block_mbswizzard.wizzard.prepare_finish_sequence = function($state) {
+M.block_mbswizzard.wizzard.prepare_finish_sequence = function(state) {
     $(this.sequence.steps[this.sequence.current_step].sel).on('click', $.proxy(function() {
-        this.finish_sequence($state);
+        this.finish_sequence(state);
     }, this));
 };
 
@@ -207,8 +246,12 @@ M.block_mbswizzard.wizzard.prepare_finish_sequence = function($state) {
  * 
  * @param string $state - finish or cancel
  */
-M.block_mbswizzard.wizzard.finish_sequence = function($state) {
-    this.set_wizzard_state($state, this.sequence.name);
+M.block_mbswizzard.wizzard.finish_sequence = function(state) {
+    this.set_wizzard_state(state, this.sequence.name);
     localStorage.removeItem(this.sequence.name);
     localStorage.removeItem('mbswizzard_current_sequence');
+    $('.tooltip').tooltip('destroy');
+    if(state === 'finish'){
+        alert('Herzlichen Glückwunsch!\nSie haben den Wizzard erfolgreich abgeschlossen.');
+    }
 };

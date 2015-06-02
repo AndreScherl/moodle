56caa8168a572df1a56d6f369b216192da806cc6
Franziska Hübler <franziska.huebler@isb.bayern.de>
2015-06-02 16:24:15 +0200

    grid format: styling, no use of trio-renderer

--- theme/mebis/renderers.php
+++ theme/mebis/renderers.php
@@ -9,7 +9,6 @@ require_once('renderers/course_renderer.php');
 require_once('renderers/core_renderer.php');
 require_once('renderers/format_topics_renderer.php');
 require_once('renderers/format_onetopic_renderer.php');
-//require_once('renderers/format_grid_renderer.php');
 require_once('renderers/format_topcoll_renderer.php');
 require_once('renderers/block_mbsmycourses_renderer.php');
 require_once('renderers/block_mbsmyschools_renderer.php');

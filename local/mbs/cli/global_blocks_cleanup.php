224d772bdaf431f5cd3f3b8a8e78c12b9992be11
Andre Scherl <andre.scherl@isb.bayern.de>
2015-06-15 17:42:47 +0200

    local mbs: changed script global_blocks_cleanup to run in cli

--- local/mbs/cli/global_blocks_cleanup.php
+++ local/mbs/cli/global_blocks_cleanup.php
@@ -23,29 +23,62 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once(dirname(__FILE__).'/../../config.php');
+define('CLI_SCRIPT', true);
+
+require_once(dirname(__FILE__).'/../../../config.php');
+require_once($CFG->libdir.'/clilib.php');
 require_once($CFG->dirroot . '/my/lib.php');
 
-require_login();
+// Get CLI options.
+list($options, $unrecognized) = cli_get_params (
+    array(
+        'resetmypages' => false,
+        'help' => false
+    ),
+    array(
+        'r' => 'resetmypages',
+        'h' => 'help'
+    )
+);
 
-$resetmypages = optional_param('resetmypages', 0, PARAM_BOOL);
+if ($options['help']) {
+    $help = 
+    "
+This script performs the following block cleanup to meet the requirements
+of the mebis redesign 2015.
 
-global $DB, $PAGE;
+* removes old dlb blocks like 'dlb', 'meinekurse', 'meineschulen', 'meinesuche'
+* removes the instances of the new blocks, which are rendered as raw blocks
+* moves all block instances of all pagetypepatterns into the region side-pre,
+  except mbsmycourses and mbsmyschools and blocks placed in side-post
+* move mbsmycourses into content region of pagetypepattern 'my-index',
+  remove the block in all other pagetypepatterns
+* move mbsmyschools into region bottom of pagetypepattern 'my-index',
+  remove the block in all other pagetypepatterns
+* optional: reset all my-pages to system default
 
-$ctx = context_system::instance();
-$PAGE->set_context($ctx);
+Options:
+ -r, --resetmypages   Reset all my-pages to default.
+ -h, --help           Print out this help.
 
-if (!has_capability('local/mbs:globalblockscleanup', $ctx)) {
-    debugging("Sorry, missing capability!");
-    die();
+Example:
+ \$sudo -u www-data /usr/bin/php local/mbs/cli/global_blocks_cleanup.php
+ \$sudo -u www-data /usr/bin/php local/mbs/cli/global_blocks_cleanup.php -r
+";
+
+    echo $help;
+    die;
 }
+
+$numberofactions = 0;
 
 // Remove the instances of the old dlb blocks 'dlb', 'meinekurse', 'meineschulen', 'meinesuche'.
 if ($blocks = $DB->get_records_select('block_instances',
         "blockname = 'dlb' OR blockname = 'meinekurse' OR blockname = 'meineschulen' OR blockname = 'meinesuche'")) {
     foreach ($blocks as $block) {
         blocks_delete_instance($block);
-        echo "Deleted instance {$block->id} of block {$block->blockname}. <br /> \n";
+        echo "Deleted instance {$block->id} of block {$block->blockname}. \n";
+        $numberofactions++;
     }
 }
 
@@ -55,7 +88,8 @@ if ($blocks = $DB->get_records_select('block_instances',
         . " OR blockname = 'mbsnewcourse' OR blockname = 'mbscoordinators'")) {
     foreach ($blocks as $block) {
         blocks_delete_instance($block);
-        echo "Deleted instance {$block->id} of block {$block->blockname}. <br /> \n";
+        echo "Deleted instance {$block->id} of block {$block->blockname}. \n";
+        $numberofactions++;
     }
 }
 
@@ -65,7 +99,8 @@ if ($blocks = $DB->get_records_select('block_instances', "blockname <> 'mbsmycou
         . " AND defaultregion <> 'side-pre' AND defaultregion <> 'side-post'")) {
     foreach ($blocks as $block) {
         $DB->set_field('block_instances', 'defaultregion', 'side-pre', array('id' => $block->id));
-        echo "Moved instance {$block->id} of block {$block->blockname} into region side-pre. <br /> \n";
+        echo "Moved instance {$block->id} of block {$block->blockname} into region side-pre. \n";
+        $numberofactions++;
     }
 }
 
@@ -77,11 +112,13 @@ if ($blocks = $DB->get_records('block_instances', array('blockname' => 'mbsmycou
                 "blockname = 'mbsmycourses' AND parentcontextid = {$block->parentcontextid} AND defaultregion = 'content'"
                 . "AND id <> {$block->id}"))) {
             blocks_delete_instance($block);
-            echo "Deleted instance {$block->id} of block {$block->blockname}. <br /> \n";
+            echo "Deleted instance {$block->id} of block {$block->blockname}. \n";
+            $numberofactions++;
         } else {
             if ($block->defaultregion !== 'content') {
                 $DB->set_field('block_instances', 'defaultregion', 'content', array('id' => $block->id));
-                echo "Moved instance {$block->id} of block {$block->blockname} into region content. <br /> \n";
+                echo "Moved instance {$block->id} of block {$block->blockname} into region content. \n";
+                $numberofactions++;
             }
         }
     }
@@ -95,22 +132,25 @@ if ($blocks = $DB->get_records('block_instances', array('blockname' => 'mbsmysch
                 "blockname = 'mbsmyschools' AND parentcontextid = {$block->parentcontextid} AND defaultregion = 'bottom' "
                 . "AND id <> {$block->id}"))) {
             blocks_delete_instance($block);
-            echo "Deleted instance {$block->id} of block {$block->blockname}. <br /> \n";
+            echo "Deleted instance {$block->id} of block {$block->blockname}. \n";
+            $numberofactions++;
         } else {
             if ($block->defaultregion !== 'bottom') {
                 $DB->set_field('block_instances', 'defaultregion', 'bottom', array('id' => $block->id));
-                echo "Moved instance {$block->id} of block {$block->blockname} into region bottom. <br /> \n";
+                echo "Moved instance {$block->id} of block {$block->blockname} into region bottom. \n";
+                $numberofactions++;
             }
         }
     }
 }
 
 // Reset all my-pages to system default, exept those with userid = null
-if ($resetmypages && $mypages = $DB->get_records_select('my_pages', "userid >= 1")) {
+if ($options['resetmypages'] && $mypages = $DB->get_records_select('my_pages', "userid >= 1")) {
     foreach ($mypages as $mypage) {
         my_reset_page($mypage->userid);
-        echo "Reset my page of userid {$mypage->userid} to default. <br /> \n";
+        echo "Reset my page of userid {$mypage->userid} to default. \n";
+        $numberofactions++;
     }
 }
 
-echo "<br />Block cleanup finished.";
+echo "Block cleanup finished with {$numberofactions} actions done.";

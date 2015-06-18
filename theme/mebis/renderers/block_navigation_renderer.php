7d6f1b9ee336b7cc33e007cac825101848ce4624
Andre Scherl <andre.scherl@isb.bayern.de>
2015-06-18 09:45:02 +0200

    theme mebis: block navigation renderer: use benefits of oop now ;-)

--- theme/mebis/renderers/block_navigation_renderer.php
+++ theme/mebis/renderers/block_navigation_renderer.php
@@ -15,20 +15,10 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Outputs the navigation tree.
+ * Overwritten renderer for block navigation of theme mebis
  *
- * @since     2.0
- * @package   block_navigation
- * @copyright 2009 Sam Hemelryk
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
-/**
- * Renderer for block navigation
- *
- * @package   block_navigation
- * @category  navigation
- * @copyright 2009 Sam Hemelryk
+ * @copyright ISB Bayern
+ * @author    Andre Scherl <andre.scherl@isb.bayern.de>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -54,114 +44,12 @@ class theme_mebis_block_navigation_renderer extends block_navigation_renderer {
             return '';
         }
         
-        // array of nested li elements
-        $lis = array();
         foreach ($items as $item) {
-            if (!$item->display && !$item->contains_active_node()) {
-                continue;
+            if ($item->key === 'courses' || $item->key === 'mycourses') {
+                $item->collapse = true;
             }
-            
-            $content = $item->get_content();
-            $title = $item->get_title();
-
-            $isexpandable = (empty($expansionlimit) || ($item->type > navigation_node::TYPE_ACTIVITY || $item->type < $expansionlimit) || ($item->contains_active_node() && $item->children->count() > 0));
-            $isbranch = $isexpandable && ($item->children->count() > 0 || ($item->has_children() && (isloggedin() || $item->type <= navigation_node::TYPE_CATEGORY)));
-
-            // Skip elements which have no content and no action - no point in showing them
-            if (!$isexpandable && empty($item->action)) {
-                continue;
-            }
-
-            $hasicon = ((!$isbranch || $item->type == navigation_node::TYPE_ACTIVITY || $item->type == navigation_node::TYPE_RESOURCE) && $item->icon instanceof renderable);
-
-            if ($hasicon) {
-                $icon = $this->output->render($item->icon);
-            } else {
-                $icon = '';
-            }
-            $content = $icon.$content; // use CSS for spacing of icons
-            if ($item->helpbutton !== null) {
-                $content = trim($item->helpbutton).html_writer::tag('span', $content, array('class'=>'clearhelpbutton'));
-            }
-
-            if ($content === '') {
-                continue;
-            }
-
-            $attributes = array();
-            if ($title !== '') {
-                $attributes['title'] = $title;
-            }
-            if ($item->hidden) {
-                $attributes['class'] = 'dimmed_text';
-            }
-            if (is_string($item->action) || empty($item->action) ||
-                    (($item->type === navigation_node::TYPE_CATEGORY || $item->type === navigation_node::TYPE_MY_CATEGORY) &&
-                    empty($options['linkcategories']))) {
-                $attributes['tabindex'] = '0'; //add tab support to span but still maintain character stream sequence.
-                $content = html_writer::tag('span', $content, $attributes);
-            } else if ($item->action instanceof action_link) {
-                //TODO: to be replaced with something else
-                $link = $item->action;
-                $link->text = $icon.$link->text;
-                $link->attributes = array_merge($link->attributes, $attributes);
-                $content = $this->output->render($link);
-                $linkrendered = true;
-            } else if ($item->action instanceof moodle_url) {
-                $content = html_writer::link($item->action, $content, $attributes);
-            }
-
-            // this applies to the li item which contains all child lists too
-            $liclasses = array($item->get_css_type(), 'depth_'.$depth);
-            $liexpandable = array();
-            // asch: We don't want to expand the courses and mycourses node
-            if ($item->has_children() && (!$item->forceopen || $item->collapse ||
-                    $item->key === 'courses' || $item->key === 'mycourses' )) {
-                $liclasses[] = 'collapsed';
-            }
-            if ($isbranch) {
-                $liclasses[] = 'contains_branch';
-                $liexpandable = array('aria-expanded' => in_array('collapsed', $liclasses) ? "false" : "true");
-            } else if ($hasicon) {
-                $liclasses[] = 'item_with_icon';
-            }
-            if ($item->isactive === true) {
-                $liclasses[] = 'current_branch';
-            }
-            $liattr = array('class' => join(' ',$liclasses)) + $liexpandable;
-            // class attribute on the div item which only contains the item content
-            $divclasses = array('tree_item');
-            if ($isbranch) {
-                $divclasses[] = 'branch';
-            } else {
-                $divclasses[] = 'leaf';
-            }
-            if ($hasicon) {
-                $divclasses[] = 'hasicon';
-            }
-            if (!empty($item->classes) && count($item->classes)>0) {
-                $divclasses[] = join(' ', $item->classes);
-            }
-            $divattr = array('class'=>join(' ', $divclasses));
-            if (!empty($item->id)) {
-                $divattr['id'] = $item->id;
-            }
-            $content = html_writer::tag('p', $content, $divattr);
-            if ($isexpandable) {
-                $content .= $this->navigation_node($item->children, array(), $expansionlimit, $options, $depth+1);
-            }
-            if (!empty($item->preceedwithhr) && $item->preceedwithhr===true) {
-                $content = html_writer::empty_tag('hr') . $content;
-            }
-            $content = html_writer::tag('li', $content, $liattr);
-            $lis[] = $content;
         }
-
-        if (count($lis)) {
-            return html_writer::tag('ul', implode("\n", $lis), $attrs);
-        } else {
-            return '';
-        }
+        
+        return parent::navigation_node($items, $attrs, $expansionlimit, $options, $depth);
     }
-
 }

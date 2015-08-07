b41ba2bb292091ef0cc3df4d67384a7620750cd3
Yair Spielmann <yair.spielmann@synergy-learning.com>
2015-08-07 11:42:42 +0100

    ALP-111 block_mbstpl meta data object

---
+++ blocks/mbstpl/classes/dataobj/meta.php
@@ -0,0 +1,72 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package block_mbstpl
+ * @copyright 2015 Yair Spielmann, Synergy Learning for ALP
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Class meta
+ * For block_mbstpl_meta.
+ * @package block_mbstpl
+ */
+namespace block_mbstpl\dataobj;
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+
+require_once($CFG->dirroot . '/completion/data_object.php');
+
+class meta extends base {
+
+    /* @var string Database table name that stores completion criteria information  */
+    public static $tablename = 'block_mbstpl_meta';
+
+    /**
+     * Array of required table fields, must start with 'id'.
+     * @var array
+     */
+    public $required_fields = array('id');
+    public $optional_fields = array(
+        'backupid' => 0,
+        'templateid' => 0,
+    );
+
+    /* @var int backupid  */
+    public $backupid;
+
+    /* @var int templateid  */
+    public $templateid;
+
+
+
+    /**
+     * Records this object in the Database, sets its id to the returned value, and returns that value.
+     * If successful this function also fetches the new object data from database and stores it
+     * in object properties.
+     *
+     * @return int PK ID if successful, false otherwise
+     */
+    public function insert() {
+        if (!($this->backupid xor $this->templateid)) {
+            throw new \coding_exception('Meta needs to be linked to either a backup or a template.');
+        }
+        parent::insert();
+    }
+}

cbb39af5f4e1ba4feeb55e261d312ae0e934ee81
Bence Laky <b.laky@intrallect.com>
2015-08-27 11:02:32 +0100

    Autocomplete added

--- blocks/mbstpl/autocomplete.php
+++ blocks/mbstpl/autocomplete.php
@@ -21,12 +21,58 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+define('AJAX_SCRIPT', true);
+
+use \block_mbstpl\dataobj\template;
+
 require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
 
 require_login();
 require_sesskey();
 
-header('Content-Type: application/json; charset=utf-8');
-$keyword = required_param("keyword", PARAM_ALPHANUM);
+global $DB;
 
-echo json_encode(array("suggestion1", "suggestion2"));
+$keyword = required_param("keyword", PARAM_ALPHANUM);
+$keyword = $DB->sql_like_escape($keyword);
+
+// Metadata suggestions.
+$sql =  'SELECT DISTINCT A.data FROM {block_mbstpl_answer} as A';
+$sql .= ' JOIN {block_mbstpl_question} as Q ON Q.id = A.questionid';
+$sql .= ' JOIN {block_mbstpl_meta} AS M ON M.id = A.metaid';
+$sql .= ' JOIN {block_mbstpl_template} as T on T.id = M.templateid';
+$sql .= ' WHERE A.data LIKE "%' . $keyword . '%" COLLATE UTF8_GENERAL_CI';
+$sql .= ' AND T.status = ?';
+$sql .= ' AND (Q.datatype = ? OR Q.datatype = ?)';
+
+$metadatasuggestions = $DB->get_fieldset_sql($sql,
+        array(template::STATUS_PUBLISHED, 'text', 'textarea'));
+
+// Course data suggestions.
+$sql = 'SELECT C.fullname, C.idnumber, C.shortname FROM {block_mbstpl_template} as T';
+$sql .= ' JOIN mdl_course as C ON T.courseid = C.id';
+$sql .= ' WHERE T.status = ?';
+$sql .= ' AND (C.fullname LIKE "%' . $keyword . '%" COLLATE UTF8_GENERAL_CI';
+$sql .= ' OR C.idnumber LIKE "%' . $keyword . '%" COLLATE UTF8_GENERAL_CI' ;
+$sql .= ' OR C.shortname LIKE "%' . $keyword . '%" COLLATE UTF8_GENERAL_CI)';
+$coursesuggestionrecords = $DB->get_records_sql($sql,
+        array(template::STATUS_PUBLISHED));
+
+$coursesuggestions = array();
+foreach ($coursesuggestionrecords as $suggestion) {
+    $coursesuggestions[] = $suggestion->fullname;
+    $coursesuggestions[] = $suggestion->idnumber;
+    $coursesuggestions[] = $suggestion->shortname;
+}
+
+// Merge the two resultset.
+$suggestions = array_merge($coursesuggestions, $metadatasuggestions);
+
+// Filter out empty values and duplicates.
+$suggestions = array_unique (array_filter ($suggestions, function($value) use ($keyword) {
+        return !!$value && strpos(strtolower($value), strtolower($keyword)) !== false;
+}));
+asort($suggestions);
+header('Content-Type: application/json; charset=utf-8');
+
+// Reset keys.
+echo json_encode(array_values($suggestions));

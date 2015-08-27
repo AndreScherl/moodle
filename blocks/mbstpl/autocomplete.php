5f928cb0f3d2c5e03da692bcc2f21a935b95b57a
Bence Laky <b.laky@intrallect.com>
2015-08-27 13:47:23 +0100

    Autocomplete LIKE escape

--- blocks/mbstpl/autocomplete.php
+++ blocks/mbstpl/autocomplete.php
@@ -8,11 +8,11 @@
 //
 // Moodle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
 
 /**
  *
@@ -20,42 +20,44 @@
  * @copyright 2015 Bence Laky <b.laky@intrallect.com>
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 define('AJAX_SCRIPT', true);
 
 use \block_mbstpl\dataobj\template;
 
-require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
+require_once (dirname(dirname(dirname(__FILE__))) . '/config.php');
 
 require_login();
 require_sesskey();
 
 global $DB;
 
-$keyword = required_param("keyword", PARAM_ALPHANUM);
+$keyword = required_param("keyword", PARAM_TEXT);
 $keyword = $DB->sql_like_escape($keyword);
+$likeKeyword = "%{$keyword}%";
 
 // Metadata suggestions.
-$sql =  'SELECT DISTINCT A.data FROM {block_mbstpl_answer} as A';
+$sql = 'SELECT DISTINCT A.data FROM {block_mbstpl_answer} as A';
 $sql .= ' JOIN {block_mbstpl_question} as Q ON Q.id = A.questionid';
 $sql .= ' JOIN {block_mbstpl_meta} AS M ON M.id = A.metaid';
 $sql .= ' JOIN {block_mbstpl_template} as T on T.id = M.templateid';
-$sql .= ' WHERE A.data LIKE "%' . $keyword . '%" COLLATE UTF8_GENERAL_CI';
+$sql .= ' WHERE ' . $DB->sql_like('A.data', '?', false);
 $sql .= ' AND T.status = ?';
 $sql .= ' AND (Q.datatype = ? OR Q.datatype = ?)';
 
 $metadatasuggestions = $DB->get_fieldset_sql($sql,
-        array(template::STATUS_PUBLISHED, 'text', 'textarea'));
+        array($likeKeyword, template::STATUS_PUBLISHED, 'text', 'textarea'
+        ));
 
 // Course data suggestions.
 $sql = 'SELECT C.fullname, C.idnumber, C.shortname FROM {block_mbstpl_template} as T';
 $sql .= ' JOIN mdl_course as C ON T.courseid = C.id';
 $sql .= ' WHERE T.status = ?';
-$sql .= ' AND (C.fullname LIKE "%' . $keyword . '%" COLLATE UTF8_GENERAL_CI';
-$sql .= ' OR C.idnumber LIKE "%' . $keyword . '%" COLLATE UTF8_GENERAL_CI' ;
-$sql .= ' OR C.shortname LIKE "%' . $keyword . '%" COLLATE UTF8_GENERAL_CI)';
+$sql .= ' AND (' . $DB->sql_like('C.fullname', '?', false);
+$sql .= ' OR ' .   $DB->sql_like('C.idnumber', '?', false);
+$sql .= ' OR ' .   $DB->sql_like('C.shortname', '?', false) . ')';
 $coursesuggestionrecords = $DB->get_records_sql($sql,
-        array(template::STATUS_PUBLISHED));
+        array(template::STATUS_PUBLISHED, $likeKeyword, $likeKeyword, $likeKeyword
+        ));
 
 $coursesuggestions = array();
 foreach ($coursesuggestionrecords as $suggestion) {
@@ -68,9 +70,11 @@ foreach ($coursesuggestionrecords as $suggestion) {
 $suggestions = array_merge($coursesuggestions, $metadatasuggestions);
 
 // Filter out empty values and duplicates.
-$suggestions = array_unique (array_filter ($suggestions, function($value) use ($keyword) {
-        return !!$value && strpos(strtolower($value), strtolower($keyword)) !== false;
-}));
+$suggestions = array_unique(
+        array_filter($suggestions,
+                function ($value) use($keyword) {
+                    return !!$value && strpos(strtolower($value), strtolower($keyword)) !== false;
+                }));
 asort($suggestions);
 header('Content-Type: application/json; charset=utf-8');
 

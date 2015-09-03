3372c09dfede162a677674f57598e456b1b72d4d
Franziska Hübler <franziska.huebler@isb.bayern.de>
2015-09-03 13:30:52 +0200

    theme mebis: increased version number

--- theme/mebis/version.php
+++ theme/mebis/version.php
@@ -8,7 +8,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2015072900;
+$plugin->version   = 2015090300;
 $plugin->requires  = 2014051200;
 $plugin->release  = 2014051300;
 $plugin->maturity  = MATURITY_BETA;

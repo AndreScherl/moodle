211c53d05a62b2fd5e843800d9f2f5d7556e6267
Andreas Wagner <awagner@gmx.de>
2015-10-19 09:53:14 +0200

    improve layout of course/management.php

--- theme/mebis/version.php
+++ theme/mebis/version.php
@@ -1,17 +1,32 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Theme Mebis version file.
+ * Theme mebis version file.
  *
- * @package theme_mebis
+ * @package    theme_mebis
+ * @copyright  2015 ISB
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2015101300;
+$plugin->version   = 2015101900;
 $plugin->requires  = 2014051200;
 $plugin->release  = 2014051300;
-$plugin->maturity  = MATURITY_BETA;
+$plugin->maturity  = MATURITY_ALPHA;
 $plugin->component = 'theme_mebis';
 $plugin->dependencies = array(
     'theme_bootstrap'  => 2014051300

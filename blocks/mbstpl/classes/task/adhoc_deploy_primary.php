c522b853839889a0de04b02d98152172a82c03f0
Janek Lasocki-Biczysko <j.lasocki-biczysko@intrallect.com>
2015-10-20 16:55:45 +0100

    ALP-117 block_mbstpl updating deploy_test.php to use adhoc_primary_task, rather than calling backup methods directly

--- blocks/mbstpl/classes/task/adhoc_deploy_primary.php
+++ blocks/mbstpl/classes/task/adhoc_deploy_primary.php
@@ -28,15 +28,25 @@ use block_mbstpl\backup;
 defined('MOODLE_INTERNAL') || die();
 
 class adhoc_deploy_primary extends \core\task\adhoc_task {
-    public function execute() {
+
+    private $courseid;
+
+    public function get_courseid() {
+        return $this->courseid;
+    }
+
+    public function execute($rethrowexception = false) {
         $bkpdetails = $this->get_custom_data();
         $backup = new \block_mbstpl\dataobj\backup(array('id' => $bkpdetails->id), true, MUST_EXIST);
         try {
             backup::backup_primary($backup);
-            $courseid = backup::restore_primary($backup);
-            \block_mbstpl\notifications::email_deployed($backup, $courseid);
+            $this->courseid = backup::restore_primary($backup);
+            \block_mbstpl\notifications::email_deployed($backup, $this->courseid);
         } catch (\moodle_exception $e) {
             \block_mbstpl\notifications::notify_error('errordeploying', $e);
+            if ($rethrowexception) {
+                throw $e;
+            }
             print_r($e->getMessage());
             print_r($e->getTrace());
             print_r($backup);

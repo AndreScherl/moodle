ba68eeb952e53e1aff5035748ee69bebbcb63f72
Yair Spielmann <yair.spielmann@synergy-learning.com>
2015-10-20 11:49:27 +0100

    ALP-119 block_mbstpl template reminder DB field

--- blocks/mbstpl/version.php
+++ blocks/mbstpl/version.php
@@ -21,7 +21,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015100700;
+$plugin->version   = 2015102000;
 $plugin->requires  = 2014051201;
 $plugin->component = 'block_mbstpl';
 $plugin->maturity  = MATURITY_STABLE;

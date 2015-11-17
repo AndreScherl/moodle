94e6a189d000376e2cf4f9d4013604647eeee09e
Janek Lasocki-Biczysko <j.lasocki-biczysko@intrallect.com>
2015-11-17 14:36:26 +0000

    ALP-143 block_mbstpl adding star rating widget to ratetemplate form

--- blocks/mbstpl/viewrating.php
+++ blocks/mbstpl/viewrating.php
@@ -64,21 +64,19 @@ if (is_null($template->rating)) {
     echo $renderer->rating($template->rating);
 
     $table = new flexible_table('block_mbstpl_ratings');
-    $table->define_columns(array('timecreated', 'rating', 'comment', 'fullname'));
+    $table->define_columns(array('timecreated', 'rating', 'fullname'));
     $table->define_headers(array(
         get_string('date'),
         get_string('rating', 'block_mbstpl'),
-        get_string('feedback', 'block_mbstpl'),
         get_string('user'),
     ));
     $table->define_baseurl($thisurl);
     $table->sortable(true, 'date', 'ASC');
-    $table->no_sorting('comment');
     $table->setup();
 
     $ufnames = get_all_user_name_fields(true, 'u');
 
-    $select = "SELECT rat.id, rat.rating, rat.comment, rat.timecreated, rat.userid, $ufnames";
+    $select = "SELECT rat.id, rat.rating, rat.timecreated, rat.userid, $ufnames";
     $from = "
     FROM {block_mbstpl_starrating} rat
     JOIN {user} u ON u.id = rat.userid
@@ -96,7 +94,6 @@ if (is_null($template->rating)) {
         $row = array();
         $row[] = userdate($result->timecreated);
         $row[] = $result->rating;
-        $row[] = $result->comment;
         $row[] = html_writer::link(new moodle_url('/user/view.php', array('id' => $result->userid)), fullname($result));
         $table->add_data($row);
     }
---
+++ blocks/mbstpl/yui/build/moodle-block_mbstpl-starrating/moodle-block_mbstpl-starrating.js
@@ -0,0 +1,83 @@
+YUI.add('moodle-block_mbstpl-starrating', function (Y, NAME) {
+
+M.block_mbstpl = M.block_mbstpl || {};
+M.block_mbstpl.starrating = {
+
+	/**
+	 * Initiate the module.
+	 */
+	init: function(radioname) {
+
+		var radios = Y.all('form input[name=' + radioname + ']'),
+			size = radios.size(),
+			emptystar = M.cfg.wwwroot + '/blocks/mbstpl/pix/emptystar.png',
+			fullstar = M.cfg.wwwroot + '/blocks/mbstpl/pix/fullstar.png',
+			labels = {};
+
+		function radioValue(radio) {
+			return parseInt(radio.get('value'), 10);
+		}
+
+		/*
+		 * Get the current rating (based on the DOM state)
+		 */
+		function getRating() {
+			for (var i = 0, radio; i < size; i++) {
+				radio = radios.item(i);
+				if (radio.get('checked')) {
+					return radioValue(radio);
+				}
+			}
+
+			return 0;
+		}
+
+		/*
+		 * Update the stars to a rating
+		 */
+		function setStars(rating) {
+			for (var r in labels) {
+				labels[r].one('img').setAttribute('src', r <= rating ? fullstar : emptystar);
+			}
+		}
+
+		/*
+		 * Resets the stars to the current rating
+		 */
+		function resetStars() {
+			var rating = getRating();
+			setStars(rating);
+		}
+
+		/*
+		 * To be used as a callback for mouseout on a star label
+		 */
+		function setStarsFromRadio() {
+			var radio = Y.one(this.getDOMNode().control);
+			var rating = radioValue(radio);
+			setStars(rating);
+		}
+
+		// Build all stars as labels and hide radio buttons.
+		for (var i = 0, startrating = getRating(); i < size; i++) {
+
+			var radio = radios.item(i),
+				value = radioValue(radio),
+				id = radio.getAttribute('id'),
+				label = Y.Node.create('<label for="' + id + '"><img src="' + (value <= startrating ? fullstar : emptystar) + '"></label>');
+
+			label.on('mouseover', setStarsFromRadio);
+			label.on('mouseout', resetStars);
+			label.setData('value', value);
+			label.appendTo(radio.ancestor());
+			labels[value] = label;
+
+			radio.setStyle('display', 'none');
+		}
+
+	}
+
+};
+
+
+}, '@VERSION@', {"requires": ["base", "node"]});

6654838564dc23d8e3ee38f51421453bcf57ab96
Andre Scherl <andre.scherl@isb.bayern.de>
2015-12-09 17:20:32 +0100

    block mbslicenseinfo: revert duplicated code of our customized license manager

--- blocks/mbslicenseinfo/classes/local/mbslicenseinfo.php
+++ blocks/mbslicenseinfo/classes/local/mbslicenseinfo.php
@@ -71,41 +71,4 @@ class mbslicenseinfo {
         
     }
     
-    /*
-     * Get all user licenses of one user
-     * 
-     * @param int $userid
-     * @return mixed - array with database records or false
-     */
-    public static function get_user_licenses($userid) {
-        global $DB;
-        if ($dbl = $DB->get_records('block_mbslicenseinfo_ul', array('userid' => $USER->id))){
-            return $dbl;
-        }
-        return false;
-    }
-    
-    /*
-     * Get system licenses 
-     * 
-     * @return array of objects of license table
-     */
-    public static function get_system_licenses() {
-        global $DB;
-        return $DB->get_records('licenses');
-    }
-
-    /*
-     * Get licenses for dropdown field in e.g. license info form
-     * 
-     * @global USER
-     * @return array - license objects
-     */
-    public static function get_licenses_for_filedropdown() {
-        global $USER;
-        $licenses = array();
-        $licenses[] = $this->get_system_licenses();
-        $licenses[] = $this->get_user_licenses($USER->id);
-        return $licenses;
-    }
 }

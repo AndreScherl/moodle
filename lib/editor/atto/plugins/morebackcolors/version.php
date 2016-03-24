4d9de9296cd21b1bb2aaf7381ffcbf6952a71f66
Andre Scherl <andre.scherl@isb.bayern.de>
2016-03-24 09:33:22 +0100

    atto plugin morebackground: decreased version number to make plugin installation possible

--- lib/editor/atto/plugins/morebackcolors/version.php
+++ lib/editor/atto/plugins/morebackcolors/version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2015081000;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2014110400;        // Requires this Moodle version.
+$plugin->requires  = 2014051209;        // Requires this Moodle version.
 $plugin->component = 'atto_morebackcolors';  // Full name of the plugin (used for diagnostics).
 $plugin->release = '1.0.0';
 $plugin->maturity = MATURITY_STABLE;

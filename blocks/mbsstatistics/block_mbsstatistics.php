9c648c911158e731aafbc5a7527fb2b445eb9ce8
René Egger <rene.egger@isb.bayern.de>
2016-08-03 11:02:51 +0200

    Blocks mbsstatistics: first draft

---
+++ blocks/mbsstatistics/block_mbsstatistics.php
@@ -0,0 +1,55 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details
+ *
+ * @package    block_mbsstatistics
+ * @copyright  René Egger <rene.egger@isb.bayern.de>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class block_mbsstatistics extends block_base {
+
+    /**
+     * Block initialization
+     */
+    public function init() {
+        $this->title = get_string('displayname', 'block_mbsstatistics');
+        $this->defaultweight = 100;
+    }
+    
+    public function get_content() {
+        if ($this->content !== null) {
+          return $this->content;
+        }
+
+        $this->content         =  new stdClass;
+        $this->content->text   = 'The content of our SimpleHTML block!';
+        $this->content->footer = 'Footer here...';
+
+        return $this->content;
+    }
+
+    /**
+     * Locations where block can be displayed
+     *
+     * @return array
+     */
+    public function applicable_formats() {
+        return array('my-index' => true);
+    }
+}

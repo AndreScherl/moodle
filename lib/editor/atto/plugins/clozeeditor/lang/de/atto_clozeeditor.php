121fb0a66bb2a11a2f11fecd3ccadee074fde579
unknown <Ostermann@PC-1059.ZFB-MS.de>
2016-08-04 11:15:40 +0200

    Getestet im Kurs "ClozeEditor - Test"
    Umgesetzt:
    *Kann TinyMCE-erstellte ClozeFragen (vollständig markieren...) und Atto-erstellte Clozefragen (irgendwo in den Span klicken) einlesen.
    *Styling passiert jetzt über CSS.
    *Ordner und Dateien: cloze vollständig durch clozeeditor ersetzt, wie ursprünglich geplant.
    
    Noch nicht umgesetzt:
    *Fragetyp NUMERICAL sollte Toleranzbereiche erlauben, hier sind es noch feste Werte.

---
+++ lib/editor/atto/plugins/clozeeditor/lang/de/atto_clozeeditor.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'atto_clozeeditor', language 'de'.
+ *
+ * @package    atto_clozeeditor
+ * @copyright  2016 Matthias Ostermann  <mail@matthias-ostermann.de>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'ClozeEditor';
+$string['addanswer'] = 'Lückentextbaustein hinzufügen';
+$string['answer'] = 'Antwort:';
+$string['createcloze'] = 'Lückentext-Baustein erstellen';
+$string['editcloze'] = 'Lückentext-Baustein bearbeiten';
+$string['feedback'] = 'Feedback:';
+$string['heading'] = 'Erstellen eines Lückentext-Bausteins';
+$string['insert'] = 'Bitte geben Sie hier die Antworten ein.';
+$string['multichoice'] = 'Auswahlliste (Dropdown)';
+$string['multichoice_h'] = 'Antwortauswahl (horizontal)';
+$string['multichoice_v'] = 'Antwortauswahl (vertikal)';
+$string['numerical'] = 'Numerisch';
+$string['percentage'] = 'Prozentzahl:';
+$string['points'] = 'Max. Punkte:';
+$string['shortanswer'] = 'Kurzantwort';
+$string['shortanswer_c'] = 'Kurzantwort (mit Groß-/Kleinschreibung)';
+$string['todo'] = 'Define possible answers';
+$string['type'] = 'Typ des Lückentext-Bausteins:';

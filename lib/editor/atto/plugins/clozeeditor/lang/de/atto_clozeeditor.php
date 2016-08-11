80dbb39ef5e03e5ab3ce3526f6c2649395426d56
Andre Scherl <andre.scherl@isb.bayern.de>
2016-08-11 15:33:57 +0200

    atto plugin clozeeditor: cloned as real repository into subdir and made some little code cosmetics

--- lib/editor/atto/plugins/clozeeditor/lang/de/atto_clozeeditor.php
+++ lib/editor/atto/plugins/clozeeditor/lang/de/atto_clozeeditor.php
@@ -1,4 +1,5 @@
 <?php
+
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -15,13 +16,12 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Strings for component 'atto_clozeeditor', language 'de'.
+ * Strings for component 'atto_clozeeditor', language 'en'.
  *
  * @package    atto_clozeeditor
  * @copyright  2016 Matthias Ostermann  <mail@matthias-ostermann.de>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 $string['pluginname'] = 'ClozeEditor';
 $string['addanswer'] = 'Lückentextbaustein hinzufügen';
 $string['answer'] = 'Antwort:';

2e812e068088b32dc314b2288eeac325e90737a3
Andre Scherl <andre.scherl@isb.bayern.de>
2016-12-07 10:58:46 +0100

    added local language strings into git repo

---
+++ lang/de_local/moodle.php
@@ -0,0 +1,47 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from https://lernplattform.mebis.bayern.de
+ *
+ * @package    core
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['adminhelpmymoodle'] = 'Blöcke für den Schreibtisch festlegen';
+$string['changepassword'] = 'Passwort';
+$string['city'] = 'Schule';
+$string['courseoverviewfiles'] = 'Bild für die Kursbeschreibung';
+$string['courseoverviewfiles_help'] = 'Bilder der Kursbeschreibung werden in der Kursübersicht zusammen mit der Kursbeschreibung angezeigt.';
+$string['courseprofiles'] = 'Besuchte Kurse';
+$string['editmyprofile'] = 'Meine Einstellungen bearbeiten';
+$string['emailempty'] = 'Der/die Teilnehmer/in hat keine Email-Adresse angegeben.';
+$string['home'] = 'Lernplattform';
+$string['messagedselectedusersfailed'] = 'Beim Versenden der Mitteilung ist ein Problem aufgetreten. Es kann sein, dass einzelne Empfänger/innen die Nachricht nicht erhalten haben, weil z.B. keine Email-Adresse angegeben wurde.';
+$string['missingcity'] = 'Schule fehlt';
+$string['myhome'] = 'Schreibtisch';
+$string['myprofile'] = 'Mein mebis';
+$string['noreplyname'] = 'mebis Lernplattform Systemnachricht';
+$string['publicprofile'] = 'Profil';
+$string['sitehome'] = 'Lernplattform IDM';
+$string['updatemymoodleoff'] = 'Schreibtisch ist fertig';
+$string['updatemymoodleon'] = 'Schreibtisch bearbeiten';
+$string['updatemyprofile'] = 'Einstellungen speichern';
+$string['usercurrentsettings'] = 'Lernplattformeinstellungen';
+$string['viewprofile'] = 'Über mich...';

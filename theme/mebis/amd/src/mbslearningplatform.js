05f77c8b8f828fa43c4b3cce9fb6a0b8f401b290
Andre Scherl <andre.scherl@isb.bayern.de>
2017-01-26 19:30:57 +0100

    theme mebis: fixed popups overlapped by sticky header

--- theme/mebis/amd/src/mbslearningplatform.js
+++ theme/mebis/amd/src/mbslearningplatform.js
@@ -9,10 +9,10 @@ require.config({
         }
     }
 });
- 
+
 define(['jquery', 'mebis'], function($, mebis) {
-    'use strict';
-    
+    //'use strict';
+
     var preventLinkDefault = function() {
         var $preventLinks = $('[data-prevent="default"]');
         $preventLinks.on('click', function() {
@@ -24,20 +24,40 @@ define(['jquery', 'mebis'], function($, mebis) {
         $('span.hiddennavbutton').click(function () {
             $(this).parent().children('ul.hiddennavleaf').toggle(200);
             $(this).parent().toggleClass('open');
-            
+
             if ($('ul.me-subnav').children('li.hiddennavnode').hasClass('open')) {
                 $('div.dropdown-inner').addClass('open');
-            } 
-            else {
+            } else {
                 $('div.dropdown-inner').removeClass('open');
             }
         });
+    };
+
+    // prevent dialog to be overlapped by sticky header
+    var repositionMoodleDialog = function() {
+        var offset = parseInt($('header.me-page-header').css("height")) + parseInt($('header.me-page-header').css("padding-top"));
+        var target = document.querySelector('body');
+        var observer = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+                mutation.addedNodes.forEach(function(node) {
+                    if (node.className.indexOf("moodle-dialogue") >= 0) {
+                        var dialog = document.querySelector(".moodle-dialogue-focused");
+                        if (parseInt(dialog.style.top) < offset) {
+                            dialog.style.top = offset + "px";
+                        }
+                    }
+                });
+            });
+        });
+        var config = {childList: true};
+        observer.observe(target, config);
     };
 
     return {
         init: function() {
             preventLinkDefault();
             hiddennav();
+            repositionMoodleDialog();
             mebis.init();
         }
     };

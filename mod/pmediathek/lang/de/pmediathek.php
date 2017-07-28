c674f32f2173c8287d960027182051d898cd0f88
Andreas Wagner <awagner@gmx.de>
2017-07-28 13:24:12 +0200

    MBS-2048: added plugin mod_pmediathek (2014071000 - ISB - SYNERGY)

---
+++ mod/pmediathek/lang/de/pmediathek.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file for Prüfungsarchiv activity
+ *
+ * @package   mod_pmediathek
+ * @copyright 2013 Davo Smith, Synergy Learning
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['displayselect'] = 'Anzeige';
+$string['displayselect_help'] = 'Anzeige für die URL einstellen. Folgende Möglichkeiten bestehen:
+
+* Einbetten - Die URL wird zusammen mit der Beschreibung auf der Seite angezeigt
+* Im Pop-Up - Die URL wird in einem neuen Browserfenster angezeigt
+';
+$string['displayselectexplain'] = 'Wählen Sie den Anzeigetyp. Es sind allerdings nicht alle Typen für alle URLs geeignet.';
+$string['modulename'] = 'Prüfungsarchiv';
+$string['modulename_help'] = 'Mit dieser Aktivität fügen Sie Ressourcen aus dem Prüfungsarchiv-Repositorium ein.';
+$string['modulenameplural'] = 'Prüfungsarchiv';
+$string['notpmediathekurl'] = 'Diese URL scheint nicht aus dem Prüfungsarchiv zu stammen';
+$string['pluginadministration'] = 'Prüfungsarchiv Administration';
+$string['pluginname'] = 'Prüfungsarchiv';

78c0305313b4ec143ed2e4dceda9a4f039f054a4
Andreas Wagner <awagner@gmx.de>
2017-07-28 11:18:22 +0200

    MBS-2048: added plugin block_mbscoordinators (2016071300 - ISB)

---
+++ blocks/mbscoordinators/lang/en/block_mbscoordinators.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * language file for Block mbscoordinators
+ *
+ * @package    block_mbscoordinators
+ * @copyright  Andre Scherl <andre.scherl@isb.bayern.de>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['mbscoordinators:addinstance'] = 'Add a mbscoordinators block';
+$string['mbscoordinators:myaddinstance'] = 'Add a mbscoordinators block to my moodle';
+$string['mbscoordinators:viewcoordinators'] = 'View list of coordinators';
+$string['mebiscoordinators'] = 'mebis coordinators';
+$string['pluginname'] = 'mebis coordinators';
+$string['sendmessage'] = 'send Messages';
+

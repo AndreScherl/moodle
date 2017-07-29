99a4f57a657a2a68ed0051213a8f4865b30899fa
Andreas Wagner <awagner@gmx.de>
2017-07-29 08:50:40 +0200

    MBS-2048: added plugin assignsubmission_geogebra (2015081701 - Christoph Stadlbauer - HACKED)

---
+++ mod/assign/submission/geogebra/lang/nl/assignsubmission_geogebra.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Strings for component 'assignsubmission_geogebra'
+ *
+ * @package        assignsubmission_geogebra
+ * @author         Christoph Stadlbauer <christoph.stadlbauer@geogebra.org>
+ * @copyright  (c) International GeoGebra Institute 2014
+ * @license        http://www.geogebra.org/license
+ */
+ 
+$string['geogebra'] = 'GeoGebra';
+$string['enabled'] = 'GeoGebra';
+$string['algebra'] = 'Algebra';
+$string['allowgeogebrasubmissions'] = 'Ingeschakeld';
+$string['cas'] = 'CAS';
+$string['default'] = 'Ingeschakeld (standaard)';
+$string['default_help'] = 'Wanneer ingeschakeld, wordt deze methode van indienen ingeschakeld voor elke nieuwe aanmelding.';
+$string['enabled_help'] = 'Wanneer ingeschakeld, kunnen studenten GeoGebra gebruiken voor hun inzending.';
+$string['eventassessableuploaded'] = 'Een GeoGebra inzending werd geupload has been uploaded:';
+$string['geogebrasubmission'] = 'GeoGebra inzending toestaan';
+$string['geometry'] = 'Meetkunde';
+$string['ggbtemplates'] = 'Kies een template';
+$string['ggbturl'] = 'GeoGebra template door de student te gebruiken';
+$string['ggbturl_help'] = 'De schermindeling of GeoGebra materiaal gebruikt als template voor de student.';
+$string['perspective3d'] = '3D Tekenvenster';
+$string['pluginname'] = 'GeoGebra inzendingen';
+$string['probCalc'] = 'Waarschijnlijkheidsrekening';
+$string['spreadsheet'] = 'Rekenblad';
+$string['userdefined'] = 'Bepaald door gebruiker:';

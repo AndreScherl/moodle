40bc157a1d65a4ff8108292ba930d1672db5fd15
Andreas Wagner <awagner@gmx.de>
2017-07-29 20:45:44 +0200

    MBS-2048: added old plugin auth_shibboleth

---
+++ auth/shibboleth/lang/de/auth_shibboleth.php
@@ -0,0 +1,32 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'auth_shibboleth', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   auth_shibboleth
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+// ...awag: additional definitions for DLB.
+$string['auth_shib_editmebisprofileurl'] = 'Link zur Änderung der persönlichen Daten';
+$string['auth_shib_editmebisprofileurlhelp'] = 'Geben sie einen Link zur Seite des Authentifizierungssystems an, auf der die persönlichen Daten geändert werden können.';
+$string['auth_shib_editusersurl'] = 'Link zur Nutzerverwaltung';
+$string['auth_shib_editusersurlhelp'] = 'Geben Sie einen Link zum Authentifizierungssystem an, unter dem berechtigte User (Nutzerverwalter) Nutzerdaten verwalten können.';
+$string['auth_shib_categorierole'] = 'Automatische Kursbereichsrolle';
+$string['auth_shib_categorierolehelp'] = 'Diese Rolle wird in der Heimatschule automatisch zugewiesen, when der user die mebisRole nutzerverwalter hat.';
+$string['nocategorierole'] = 'keine Rolle zuweisen';
